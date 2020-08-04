9d724de240ba998613893351251cb9f29f61dd78
Pavel Marchuk <pavel.marchuk@innowise-group.com>
2020-08-04 18:48:23 +0300

    init commit

--- spring-cloud-client/src/main/java/com/innowise/springcloudclient/SpringCloudClientApplication.java
+++ spring-cloud-client/src/main/java/com/innowise/springcloudclient/SpringCloudClientApplication.java
@@ -1,14 +1,45 @@
 package com.innowise.springcloudclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
-@EnableConfigClient
+@RestController
+@RefreshScope
 public class SpringCloudClientApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudClientApplication.class, args);
 	}
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+
+	@Value("${user.role}")
+	private String name;
+
+	@Value("${user.password}")
+	private String password;
+
+	@RequestMapping("/name")
+	String getValue() {
+		return this.name + " " + this.password;
+	}
 }
